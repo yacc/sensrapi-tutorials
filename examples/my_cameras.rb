b3c8b229218cd76a47d0e4177ee51c3bc49da3a6
Yacin Bahi <yacin@sensr.net>
2013-01-28 12:40:55 -0800

    fix examples

--- examples/my_cameras.rb
+++ examples/my_cameras.rb
@@ -1,8 +1,8 @@
 require 'rubygems'
 require 'sensr'
-require 'googlecharts'
 
-Sensr.oauth_token = REPLACE_WITH_YOUR_OAUTH_TOKEN
+Sensr.oauth_token = "02e79d6b069589aeff34dd03bb050c20e01901eb4a59a4a4c6bcbd271a949067"
 RestClient.log    = $stderr
 
-c = Sensr::Camera.owned
+mycams = Sensr::Camera.owned
+mycams.each {|c| puts  "camera: " + c.attributes["id"].to_s + ' ' + c.attributes["name"] + ' ' + c.attributes["accessibility"] }
--- examples/timelapse.rb
+++ examples/timelapse.rb
@@ -2,11 +2,11 @@ require 'rubygems'
 require 'sensr'
 require 'pp'
 
-Sensr.oauth_token = REPLACE_WITH_YOUR_OAUTH_TOKEN
+Sensr.oauth_token = "02e79d6b069589aeff34dd03bb050c20e01901eb4a59a4a4c6bcbd271a949067"
 target_camera     = ARGV[0].to_i
 RestClient.log    = $stderr
 
-c = Sensr::Camera.find(MYCAMERA)
+c = Sensr::Camera.find(target_camera)
 
 c.week(Time.now.to_i - (86400 * 5))["week"]["days"].each do |day|
   day["day"]["hours"].each do |hour|
